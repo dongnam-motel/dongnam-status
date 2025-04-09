8f25a819540263f6af16432961afecb9a718c691
dongnam-motel <ugotlove@gmail.com>
2025-04-10 06:25:32 +0900

    Add files via upload

---
+++ main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import RoomStatusBoard from './RoomStatusBoard'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RoomStatusBoard />
+  </React.StrictMode>,
+)
